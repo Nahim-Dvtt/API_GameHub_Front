d190eff8b7cbc5f83c4b94947fef84918d222e49
Nahim-Dvtt <nahim.davitti@hotmail.fr>
2025-11-24 15:54:14 +0100

    Update GAME_API URL to production endpoint

--- public/js/games.js
+++ public/js/games.js
@@ -1,4 +1,4 @@
-const GAME_API = 'http://localhost:4000/api/games';
+const GAME_API = 'https://api-gamehub-front.onrender.com/api/games';
 
 document.addEventListener('DOMContentLoaded', () => {
   if (!document.getElementById('games')) return;
