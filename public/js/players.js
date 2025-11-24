aa5c9f073e26df3723740d87f5c75fbcaec39dc1
Nahim-Dvtt <nahim.davitti@hotmail.fr>
2025-11-24 16:10:58 +0100

    Update players.js

--- public/js/players.js
+++ public/js/players.js
@@ -1,5 +1,5 @@
 (() => {
-  const PLAYER_API = 'https://api-gamehub-front.onrender.com/api/players';
+  const PLAYER_API = 'https://api-gamehub-back.onrender.com/api/players';
 
   async function loadPlayers() {
     const container = document.getElementById('players');
